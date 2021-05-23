fcc8163abd0cd0b206b99cf1a110b2a176fc3172
FunnyWolf <yu5890681@gmail.com>
2021-05-23 17:56:29 +0800

    更新1.3.8

--- src/pages/Core/SystemSetting.js
+++ src/pages/Core/SystemSetting.js
@@ -43,8 +43,8 @@ const { Option } = Select;
 const { TabPane } = Tabs;
 const { Title, Paragraph, Text } = Typography;
 
-const viper_version = 'v1.3.7';
-const viper_update_date = '2021-05-16';
+const viper_version = 'v1.3.8';
+const viper_update_date = '2021-05-23';
 const datas = [
   {
     name: 'metasploit-framework',
