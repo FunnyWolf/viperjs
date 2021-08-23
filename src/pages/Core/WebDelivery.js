6032e77af706f91fbf6edcff72f0a6cf40cf5b38
rootkit <yu5890681@gmail.com>
2021-08-23 10:09:07 +0800

    webdelivery新增拓展选项

--- src/pages/Core/WebDelivery.js
+++ src/pages/Core/WebDelivery.js
@@ -1,12 +1,12 @@
-import React, { Fragment, memo, useImperativeHandle, useState } from "react";
+import React, { Fragment, memo, useImperativeHandle, useState } from 'react';
 import {
   deleteMsgrpcWebDeliveryAPI,
   getCoreSettingAPI,
   getMsgrpcWebDeliveryAPI,
-  postMsgrpcWebDeliveryAPI
-} from "@/services/apiv1";
+  postMsgrpcWebDeliveryAPI,
+} from '@/services/apiv1';
 // import { useControllableValue, useBoolean, useMount } from '@umijs/hooks';
-import styles from "./WebDelivery.less";
+import styles from './WebDelivery.less';
 import {
   Button,
   Card,
@@ -24,17 +24,17 @@ import {
   Space,
   Table,
   Tooltip,
-  Typography
-} from "antd";
-import { CloudDownloadOutlined, SyncOutlined } from "@ant-design/icons";
-import { useRequest } from "umi";
+  Typography,
+} from 'antd';
+import { CloudDownloadOutlined, SyncOutlined } from '@ant-design/icons';
+import { useRequest } from 'umi';
 
 const { Panel } = Collapse;
 const { Option } = Select;
 
 const randomString = (length) => {
-  let result = "";
-  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = '';
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 };
@@ -43,28 +43,29 @@ const CreateWebDeliveryModalContent = props => {
   const { createWebDeliveryFinish } = props;
   const formLayoutLong = {
     labelCol: { span: 6 },
-    wrapperCol: { span: 16 }
+    wrapperCol: { span: 16 },
   };
   const formLayoutShort = {
     labelCol: { span: 6 },
-    wrapperCol: { span: 10 }
+    wrapperCol: { span: 10 },
   };
   const formLayout = {
     labelCol: { span: 6 },
-    wrapperCol: { span: 14 }
+    wrapperCol: { span: 14 },
   };
   const buttonLayout = {
-    wrapperCol: { offset: 5, span: 14 }
+    wrapperCol: { offset: 5, span: 14 },
   };
 
-  const [selectPayload, setStateSelectPayload] = useState(null);
+  const [selectPayload, setSelectPayload] = useState(null);
+  const [selectTarget, setSelectTarget] = useState(null);
   const [pem_files, setPemfiles] = useState([]);
   const [lhost, setLhost] = useState(null);
   const [handlerConf, setHandlerConf] = useState([]);
 
-  useRequest(() => getCoreSettingAPI({ kind: "lhost" }), {
+  useRequest(() => getCoreSettingAPI({ kind: 'lhost' }), {
     onSuccess: (result, params) => {
-      if (result.lhost === null || result.lhost === "") {
+      if (result.lhost === null || result.lhost === '') {
         setLhost(location.hostname);
       } else {
         setLhost(result.lhost);
@@ -72,15 +73,15 @@ const CreateWebDeliveryModalContent = props => {
       setPemfiles(result.pem_files);
     },
     onError: (error, params) => {
-    }
+    },
   });
 
-  useRequest(() => getCoreSettingAPI({ kind: "handlerconf" }), {
+  useRequest(() => getCoreSettingAPI({ kind: 'handlerconf' }), {
     onSuccess: (result, params) => {
       setHandlerConf(result);
     },
     onError: (error, params) => {
-    }
+    },
   });
 
   const createWebDeliveryReq = useRequest(postMsgrpcWebDeliveryAPI, {
@@ -89,7 +90,7 @@ const CreateWebDeliveryModalContent = props => {
       createWebDeliveryFinish();
     },
     onError: (error, params) => {
-    }
+    },
   });
 
   const onCreateWebDeliveryBySubmit = params => {
@@ -98,57 +99,73 @@ const CreateWebDeliveryModalContent = props => {
 
   const changePayloadOption = (value, selectedOptions) => {
     const onehandler = JSON.parse(value);
-    setStateSelectPayload(onehandler.PAYLOAD);
+    setSelectPayload(onehandler.PAYLOAD);
   };
+
+  const changeTargetOption = (value, selectedOptions) => {
+    setSelectTarget(value);
+  };
+
 
   const webDeliveryBaseOption = () => {
     let options = [];
     if (selectPayload === null || selectPayload === undefined) {
       return null;
     }
-    if (selectPayload.includes("windows")) {
+    if (selectPayload.includes('windows')) {
       options.push(<FormNew.Item
           {...formLayoutShort}
           rules={[
             {
               required: true,
-              message: "请选择Target"
-            }
+              message: '请选择Target',
+            },
           ]}
           label={
             <Tooltip title="请注意Target需要和Payload保持OS一致,如PSH只适用于Windows类型载荷">
               <span>Target</span>
             </Tooltip>
           }
+
           name="TARGET"
         >
-          <Select placeholder="请选择Target" allowClear>
+          <Select
+            placeholder="请选择Target"
+            onChange={changeTargetOption}
+            allowClear
+          >
             <Option value={2}>PSH</Option>
             <Option value={3}>Regsvr32</Option>
             <Option value={4}>pubprn</Option>
             <Option value={5}>SyncAppvPublishingServer</Option>
             <Option value={6}>PSH (Binary)</Option>
           </Select>
-        </FormNew.Item>
+        </FormNew.Item>,
       );
 
-    } else if (selectPayload.includes("linux")) {
+    } else if (selectPayload.includes('linux')) {
       options.push(<FormNew.Item
         {...formLayoutShort}
         rules={[
           {
             required: true,
-            message: "请选择Target"
-          }
+            message: '请选择Target',
+          },
         ]}
         label={
           <Tooltip title="请注意Target需要和Payload保持OS一致,如PSH只适用于Windows类型载荷">
             <span>Target</span>
           </Tooltip>
         }
+        onChange={changeTargetOption}
+        allowClear
         name="TARGET"
       >
-        <Select placeholder="请选择Target" allowClear>
+        <Select
+          placeholder="请选择Target"
+          onChange={changeTargetOption}
+          allowClear
+        >
           <Option value={7}>Linux</Option>
         </Select>
       </FormNew.Item>);
@@ -175,12 +192,12 @@ const CreateWebDeliveryModalContent = props => {
         rules={[
           {
             required: true,
-            message: "请输入web服务监听的IP地址"
-          }
+            message: '请输入web服务监听的IP地址',
+          },
         ]}
       >
-        <Input placeholder="请输入web服务监听的IP地址" />
-      </FormNew.Item>
+        <Input placeholder="请输入web服务监听的IP地址"/>
+      </FormNew.Item>,
     );
     options.push(
       <FormNew.Item
@@ -190,12 +207,12 @@ const CreateWebDeliveryModalContent = props => {
         rules={[
           {
             required: true,
-            message: "请输入端口"
-          }
+            message: '请输入端口',
+          },
         ]}
       >
-        <InputNumber style={{ width: 160 }} />
-      </FormNew.Item>
+        <InputNumber style={{ width: 160 }}/>
+      </FormNew.Item>,
     );
 
     options.push(
@@ -206,8 +223,8 @@ const CreateWebDeliveryModalContent = props => {
         valuePropName="checked"
         rules={[]}
       >
-        <Checkbox />
-      </FormNew.Item>
+        <Checkbox/>
+      </FormNew.Item>,
     );
     options.push(
       <FormNew.Item
@@ -225,7 +242,7 @@ const CreateWebDeliveryModalContent = props => {
             <Option value={`~/.msf4/loot/${encoder}`}>{encoder}</Option>
           ))}
         </Select>
-      </FormNew.Item>
+      </FormNew.Item>,
     );
     options.push(
       <FormNew.Item
@@ -238,9 +255,38 @@ const CreateWebDeliveryModalContent = props => {
         initialValue={randomString(8)}
         name="URIPATH"
       >
-        <Input placeholder="请输入自定义的URI" />
-      </FormNew.Item>
+        <Input placeholder="请输入自定义的URI"/>
+      </FormNew.Item>,
     );
+
+    if (options.length === 0) {
+      return null;
+    } else {
+      return options;
+    }
+  };
+
+  const webDevliverAdvanceOption = () => {
+    let options = [];
+    if (selectTarget === null || selectTarget === undefined) {
+      return null;
+    }
+    console.log(selectTarget);
+    if (selectTarget === 2) {
+      options.push(
+        <FormNew.Item
+          {...formLayout}
+          label="AMSI/SBL bypass"
+          name="Powershell::prepend_protections_bypass"
+          valuePropName="checked"
+          initialValue={true}
+          rules={[]}
+        >
+          <Checkbox/>
+        </FormNew.Item>,
+      );
+    }
+
 
     if (options.length === 0) {
       return null;
@@ -250,15 +296,15 @@ const CreateWebDeliveryModalContent = props => {
   };
   return (
     <FormNew onFinish={onCreateWebDeliveryBySubmit}>
-      <Collapse bordered={false} defaultActiveKey={["base", "advance"]}>
+      <Collapse bordered={false} defaultActiveKey={['base', 'web']}>
         <Panel header="载荷参数" key="base">
           <FormNew.Item
             {...formLayoutLong}
             rules={[
               {
                 required: true,
-                message: "请选择监听"
-              }
+                message: '请选择监听',
+              },
             ]}
             label={<span>监听</span>}
             name="handlerconf"
@@ -274,15 +320,18 @@ const CreateWebDeliveryModalContent = props => {
           </FormNew.Item>
           {webDeliveryBaseOption()}
         </Panel>
-        <Panel header="Web参数" key="advance">
+        <Panel header="Web参数" key="web">
           {webDevliverServerOption()}
+        </Panel>
+        <Panel header="拓展参数" key="advance">
+          {webDevliverAdvanceOption()}
         </Panel>
       </Collapse>
       <FormNew.Item style={{ marginTop: 24 }} {...buttonLayout}>
         <Button
           block
           loading={createWebDeliveryReq.loading}
-          icon={<CloudDownloadOutlined />}
+          icon={<CloudDownloadOutlined/>}
           htmlType="submit"
           type="primary"
         >
@@ -297,10 +346,10 @@ const showHandlerDetail = item => {
   const Descriptions_Items = [];
   let showstr = null;
   for (const key in item) {
-    if (item[key] === null || item[key] === "") {
+    if (item[key] === null || item[key] === '') {
       continue;
     } else if (item[key] === true || item[key] === false) {
-      showstr = item[key] ? "True" : "False";
+      showstr = item[key] ? 'True' : 'False';
     } else {
       showstr = item[key];
     }
@@ -309,8 +358,8 @@ const showHandlerDetail = item => {
   Modal.info({
     mask: false,
     style: { top: 20 },
-    width: "70%",
-    icon: "",
+    width: '70%',
+    icon: '',
     content: (
       <Descriptions
         style={{ marginTop: -32, marginRight: -24, marginLeft: -24, marginBottom: -16 }}
@@ -322,12 +371,12 @@ const showHandlerDetail = item => {
       </Descriptions>
     ),
     onOk() {
-    }
+    },
   });
 };
 
 const WebDelivery = (props) => {
-  console.log("WebDelivery");
+  console.log('WebDelivery');
   const [createWebDeliveryModalVisible, setCreateWebDeliveryModalVisible] = useState(false);
   const [webDeliveryList, setWebDeliveryList] = useState([]);
 
@@ -336,7 +385,7 @@ const WebDelivery = (props) => {
     return {
       updateData: () => {
         listWebDeliveryReq.run();
-      }
+      },
     };
   });
 
@@ -346,7 +395,7 @@ const WebDelivery = (props) => {
       setWebDeliveryList(result);
     },
     onError: (error, params) => {
-    }
+    },
   });
 
   const listWebDeliveryReq = useRequest(getMsgrpcWebDeliveryAPI, {
@@ -355,7 +404,7 @@ const WebDelivery = (props) => {
       setWebDeliveryList(result);
     },
     onError: (error, params) => {
-    }
+    },
   });
 
   const createWebDeliveryReq = useRequest(postMsgrpcWebDeliveryAPI, {
@@ -364,7 +413,7 @@ const WebDelivery = (props) => {
       listWebDeliveryReq.run();
     },
     onError: (error, params) => {
-    }
+    },
   });
 
 
@@ -374,7 +423,7 @@ const WebDelivery = (props) => {
       listWebDeliveryReq.run();
     },
     onError: (error, params) => {
-    }
+    },
   });
 
 
@@ -390,7 +439,7 @@ const WebDelivery = (props) => {
         <Col span={12}>
           <Button
             block
-            icon={<CloudDownloadOutlined />}
+            icon={<CloudDownloadOutlined/>}
             onClick={() => setCreateWebDeliveryModalVisible(true)}
           >
             新增服务
@@ -398,7 +447,7 @@ const WebDelivery = (props) => {
         </Col>
         <Col span={12}>
           <Button
-            icon={<SyncOutlined />}
+            icon={<SyncOutlined/>}
             block
             loading={listWebDeliveryReq.loading || createWebDeliveryReq.loading || destoryWebDeliveryReq.loading}
             onClick={() => listWebDeliveryReq.run()}
@@ -417,30 +466,30 @@ const WebDelivery = (props) => {
         dataSource={webDeliveryList}
         columns={[
           {
-            title: "URL",
-            dataIndex: "URL",
-            key: "URL",
+            title: 'URL',
+            dataIndex: 'URL',
+            key: 'URL',
             width: 320,
-            render: (text, record) => record.URL
+            render: (text, record) => record.URL,
           },
           {
-            title: "Target",
-            dataIndex: "Target-Name",
-            key: "Target-Name",
+            title: 'Target',
+            dataIndex: 'Target-Name',
+            key: 'Target-Name',
             width: 120,
-            render: (text, record) => record["Target-Name"]
+            render: (text, record) => record['Target-Name'],
           },
           {
-            title: "载荷",
-            dataIndex: "PAYLOAD",
-            key: "PAYLOAD",
+            title: '载荷',
+            dataIndex: 'PAYLOAD',
+            key: 'PAYLOAD',
             width: 280,
-            render: (text, record) => record.PAYLOAD
+            render: (text, record) => record.PAYLOAD,
           },
           {
-            title: "LHOST/RHOST",
-            dataIndex: "PAYLOAD",
-            key: "PAYLOAD",
+            title: 'LHOST/RHOST',
+            dataIndex: 'PAYLOAD',
+            key: 'PAYLOAD',
             width: 160,
             render: (text, record) => {
               if (record.RHOST !== undefined && record.RHOST !== null) {
@@ -450,26 +499,26 @@ const WebDelivery = (props) => {
                 return <span>{record.LHOST}</span>;
               }
               return null;
-            }
+            },
           },
           {
-            title: "PORT",
-            dataIndex: "LPORT",
-            key: "LPORT",
+            title: 'PORT',
+            dataIndex: 'LPORT',
+            key: 'LPORT',
             width: 64,
-            render: (text, record) => <span>{record.LPORT}</span>
+            render: (text, record) => <span>{record.LPORT}</span>,
           },
           {
-            title: "载荷参数",
-            dataIndex: "PAYLOAD",
-            key: "PAYLOAD",
+            title: '载荷参数',
+            dataIndex: 'PAYLOAD',
+            key: 'PAYLOAD',
             render: (text, record) => {
               const items = [];
               if (record.LURI !== undefined && record.LURI !== null) {
                 items.push(<span>{` LURI: ${record.LURI}`}</span>);
               }
               if (record.HandlerSSLCert !== undefined && record.HandlerSSLCert !== null) {
-                const pos = record.HandlerSSLCert.lastIndexOf("/");
+                const pos = record.HandlerSSLCert.lastIndexOf('/');
                 items.push(<span>{` 证书: ${record.HandlerSSLCert.substr(pos + 1)}`}</span>);
               }
 
@@ -489,54 +538,54 @@ const WebDelivery = (props) => {
               if (record.SERVER_ID !== undefined && record.SERVER_ID !== null) {
                 items.push(<span>{` SERVER_ID: ${record.SERVER_ID}`}</span>);
               }
-              return <Space style={{ display: "flex" }}>{items}</Space>;
-            }
+              return <Space style={{ display: 'flex' }}>{items}</Space>;
+            },
           },
           {
-            title: "操作",
-            dataIndex: "operation",
+            title: '操作',
+            dataIndex: 'operation',
             width: 216,
             render: (text, record) => {
               return (
-                <div style={{ textAlign: "center" }}>
+                <div style={{ textAlign: 'center' }}>
                   <Space size="middle">
                     <Popover placement="left" title={text} trigger="click"
                              content={
                                <Card
-                                 style={{ width: "50vw" }}
+                                 style={{ width: '50vw' }}
                                >
                                  <Typography.Text
                                    copyable
-                                 >{record["ONE-LINE-CMD"]}</Typography.Text></Card>
+                                 >{record['ONE-LINE-CMD']}</Typography.Text></Card>
                              }
                     >
-                      <a style={{ color: "#faad14" }}>一句话执行</a>
+                      <a style={{ color: '#faad14' }}>一句话执行</a>
                     </Popover>
                     <a onClick={() => showHandlerDetail(record)}>详细参数</a>
                     <a
                       onClick={() => destoryWebDeliveryReq.run({ jobid: record.ID })}
-                      style={{ color: "red" }}
+                      style={{ color: 'red' }}
                     >
                       删除
                     </a>
                   </Space>
                 </div>
               );
-            }
-          }
+            },
+          },
         ]}
       />
       <Modal
         // title="添加监听"
         style={{ top: 20 }}
         width="60vw"
-        bodyStyle={{ padding: "0px 0px 1px 0px" }}
+        bodyStyle={{ padding: '0px 0px 1px 0px' }}
         destroyOnClose
         visible={createWebDeliveryModalVisible}
         footer={null}
         onCancel={() => setCreateWebDeliveryModalVisible(false)}
       >
-        <CreateWebDeliveryModalContent createWebDeliveryFinish={createWebDeliveryFinish} />
+        <CreateWebDeliveryModalContent createWebDeliveryFinish={createWebDeliveryFinish}/>
       </Modal>
     </Fragment>
   );
