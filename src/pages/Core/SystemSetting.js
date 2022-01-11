86784835d6780a4aa18d022aac0945e8af9e3c4a
FunnyWolf <yu5890681@gmail.com>
2022-01-11 21:22:13 +0800

    update 1.5.13

--- src/pages/Core/SystemSetting.js
+++ src/pages/Core/SystemSetting.js
@@ -49,8 +49,8 @@ const { Option } = Select;
 const { TabPane } = Tabs;
 const { Title, Paragraph, Text } = Typography;
 
-const viper_version = "v1.5.12";
-const viper_update_date = "2021-12-31";
+const viper_version = "v1.5.13";
+const viper_update_date = "2021-01-11";
 
 
 const buttonItemLayout = {
