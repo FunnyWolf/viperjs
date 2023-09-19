abc149d017b293241370e54ab361c9fb28f379dd
rootkit <yu5890681@gmail.com>
2023-09-19 16:22:09 +0800

    add feature https://github.com/FunnyWolf/Viper/issues/170

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// https://umijs.org/config/
+// import webpackPlugin from './plugin.config';
+// const compactTheme = require('antd/dist/compact-theme');
+export const HostIP = "wolf";
+export const Version = "${DATETIME}";
