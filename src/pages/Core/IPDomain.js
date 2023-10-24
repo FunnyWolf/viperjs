ab1db7c6fc3c709a849f8447162ddd72e4f5dd05
rootkit <yu5890681@gmail.com>
2023-10-24 16:53:48 +0800

    tmp store

--- src/pages/Core/IPDomain.js
+++ src/pages/Core/IPDomain.js
@@ -2,19 +2,16 @@ import React, { Fragment, memo, useImperativeHandle, useState } from 'react'
 import moment from 'moment'
 import { getLocale, useRequest } from 'umi'
 import {
-    deleteMsgrpcFileMsfAPI,
-    getCoreCurrentUserAPI,
-    getMsgrpcFileMsfAPI,
-    postPostmodulePostModuleActuatorAPI,
+    deleteMsgrpcFileMsfAPI, getCoreCurrentUserAPI, getMsgrpcFileMsfAPI, postPostmodulePostModuleActuatorAPI,
 } from '@/services/apiv1'
 
-import { Button, Card, Col, Modal, Row, Space, Table, Tag, Upload } from 'antd'
+import { Button, Card, Col, Modal, Row, Space, Table, Tag, Upload, List, Typography } from 'antd'
 import { CopyOutlined, SyncOutlined, UploadOutlined } from '@ant-design/icons'
 import copy from 'copy-to-clipboard'
 import { getToken } from '@/utils/authority'
 import { cssCalc, Downheight } from '@/utils/utils'
 import {
-    formatText, getSessionlocate, manuali18n, msgerror, msgsuccess, msgwarning,
+    formatText, getModuleName, getSessionlocate, manuali18n, msgerror, msgsuccess, msgwarning,
 } from '@/utils/locales'
 import { DocIcon, MyIcon, WebMainHeight } from '@/pages/Core/Common'
 import { useModel } from '@@/plugin-model/useModel'
@@ -24,7 +21,7 @@ import { useInterval } from 'ahooks'
 import {
     CaretRightOutlined, SubnodeOutlined,
 } from '@ant-design/icons'
-import { Dropdown, Tooltip } from '_antd@4.24.14@antd'
+import { Dropdown, Tooltip } from 'antd'
 
 let protocol = 'ws://'
 let webHost = HostIP + ':8002'
@@ -38,8 +35,7 @@ const IPDomain = props => {
     const {
         setHeatbeatsocketalive, heatbeatsocketalive,
     } = useModel('HostAndSessionModel', model => ({
-        setHeatbeatsocketalive: model.setHeatbeatsocketalive,
-        heatbeatsocketalive: model.heatbeatsocketalive,
+        setHeatbeatsocketalive: model.setHeatbeatsocketalive, heatbeatsocketalive: model.heatbeatsocketalive,
     }))
 
     const {
@@ -48,73 +44,85 @@ const IPDomain = props => {
         ipdomains: model.ipdomains, setIPDomains: model.setIPDomains,
     }))
 
-    // useImperativeHandle(props.onRef, () => {
-    //     return {
-    //         updateData: () => {
-    //             listFileMsfReq.run()
-    //         },
-    //     }
-    // })
 
-    // useRequest(getMsgrpcFileMsfAPI, {
-    //     onSuccess: (result, params) => {
-    //         setFileMsfListActive(result)
-    //     }, onError: (error, params) => {
-    //     },
-    // })
+    const IPDomainTable = () => {
+        const renderItem = item => {
+            return <List.Item
+                key={item.id}
+                style={{ padding: 4 }}
+            >
+                <Row>
+                    <Space>
+                        <Tag
+                            color="cyan"
+                            style={{
+                                // marginLeft: -1,
+                                // marginRight: 4,
+                                textAlign: 'center',
+                            }}
+                        >
+                            <strong>{moment(item.update_time * 1000).format('MM-DD HH:mm:ss')}</strong>
+                        </Tag>
+                        <Tag
+                            color="blue"
+                            style={{
+                                textAlign: 'center',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <strong>{item.ipdomain}</strong>
+                        </Tag>
+                        <Tag
+                            color="green"
+                            style={{
+                                // width: 160,
+                                textAlign: 'center',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <strong>{item.port}</strong>
+                        </Tag>
+                        <Tag
+                            color="cyan"
+                            style={{
+                                // width: 160,
+                                textAlign: 'center',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <strong>{item.service}</strong>
+                        </Tag>
+                    </Space>
+                </Row>
+                <Row>
+                    <Col span={12}>
 
-    // const listFileMsfReq = useRequest(getMsgrpcFileMsfAPI, {
-    //     manual: true, onSuccess: (result, params) => {
-    //         setFileMsfListActive(result)
-    //     }, onError: (error, params) => {
-    //     },
-    // })
+                    </Col>
+                    <Col span={12}>
 
-    const portServiceRowRender = portServiceRecord => {
-        const portServiceTableColumns = [
-            {
-                dataIndex: 'ipaddress',
-                width: 128,
-                render: (text, portServiceRecord) => {
-                    const session = portServiceRecord
+                    </Col>
+                </Row>
+            </List.Item>
+        }
 
-                    const hostwithsession = JSON.parse(
-                        JSON.stringify(portServiceRecord)) // deep copy
-                    hostwithsession.session = session
 
-                    return (<Button
-                        // onClick={() => {
-                        //     setRunModuleModalVisable(true)
-                        //     setActiveHostAndSession(hostwithsession)
-                        // }}
-                        style={{
-                            width: 104,
-                            backgroundColor: '#274916',
-                            textAlign: 'center',
-                            cursor: 'pointer',
-                        }}
-                        size="small"
-                    >
-                        <CaretRightOutlined />
-                    </Button>)
-                },
-            }]
-
-        return <Table
-            loading={!heatbeatsocketalive}
-            dataSource={portServiceRecord.portservice}
-            // style={{ marginLeft: 23 }}
+        return <List
+            bordered
+            style={{
+                overflow: 'auto',
+                maxHeight: cssCalc(`${WebMainHeight} - 96px`),
+                minHeight: cssCalc(`${WebMainHeight} - 96px`),
+            }}
+            itemLayout="vertical"
             size="small"
-            columns={portServiceTableColumns}
-            rowKey={item => item.id}
-            pagination={false}
-            showHeader={false}
-            locale={{ emptyText: null }}
-        />
+            dataSource={ipdomains}
+            renderItem={item => renderItem(item)}
+        >
+        </List>
     }
 
     return (<Fragment>
-        <DocIcon url="https://www.yuque.com/vipersec/help/yc0ipk" />
+        <DocIcon url="https://www.yuque.com/vipersec/help/yc0ipk"/>
         <Row
             gutter={0}
             style={{
@@ -123,7 +131,7 @@ const IPDomain = props => {
         ><Col span={4}>
             <Button
                 block
-                icon={<SyncOutlined />}
+                icon={<SyncOutlined/>}
             >
                 {formatText('app.core.refresh')}
             </Button>
@@ -131,102 +139,13 @@ const IPDomain = props => {
             <Col span={8}>
                 <Button
                     block
-                    icon={<SyncOutlined />}
+                    icon={<SyncOutlined/>}
                 >
                     {formatText('app.core.refresh')}
                 </Button>
             </Col>
         </Row>
-        <Table
-            virtual
-            style={{
-                overflow: 'auto',
-                // margin: 0,
-                maxHeight: cssCalc(`${WebMainHeight} - 64px`),
-                minHeight: cssCalc(`${WebMainHeight} - 64px`),
-            }}
-            loading={!heatbeatsocketalive}
-            scroll={{
-                y: cssCalc(`${WebMainHeight} - 96px`),
-            }}
-            size="small"
-            bordered
-            pagination={false}
-            rowKey="id"
-            columns={[
-                {
-                    title: formatText('app.filemsf.filename'),
-                    dataIndex: 'ip',
-                    key: 'ip',
-                    width: 112,
-                    sorter: (a, b) => a.ip >= b.ip,
-                    render: (text, record) => <span>{record.ip}</span>,
-                },
-                {
-                    title: formatText('app.filemsf.filename'),
-                    dataIndex: 'domain',
-                    key: 'domain',
-                    width: 240,
-                    sorter: (a, b) => a.domain >= b.domain,
-                    render: (text, record) => <span>{record.domain}</span>,
-                },
-                // {
-                //     title: formatText('app.filemsf.filename'),
-                //     dataIndex: 'source',
-                //     key: 'source',
-                //     sorter: (a, b) => a.source >= b.source,
-                //     render: (text, record) => <span>{record.source}</span>,
-                // },
-                // {
-                //     title: formatText('app.filemsf.size'),
-                //     dataIndex: 'source_key',
-                //     key: 'source_key',
-                //     sorter: (a, b) => a.size >= b.size,
-                // },
-                {
-                    title: formatText('app.core.updatetime'),
-                    key: 'ip',
-                    dataIndex: 'port_and_service',
-                    render: (text, record) => {
-                        const port_and_service_tags = []
-                        record.port_and_service.forEach(
-                            one_port_and_service => {
-                                // 心跳标签
-                                let one_tag = <Tag
-                                    color="orange"
-                                    style={{
-                                        textAlign: 'center', cursor: 'pointer',
-                                    }}
-                                >
-                                    {one_port_and_service.port} {one_port_and_service.service}
-                                </Tag>
-                                port_and_service_tags.push(one_tag)
-                            })
-                        return (<div
-                            style={{
-                                display: 'flex', cursor: 'pointer',
-                            }}
-                        >{port_and_service_tags}
-                        </div>)
-                    },
-                },
-                {
-                    title: formatText('app.core.updatetime'),
-                    dataIndex: 'update_time',
-                    key: 'update_time',
-                    width: 118,
-                    sorter: (a, b) => a.update_time >= b.update_time,
-                    render: (text, record) => (
-                        <Tag color="cyan">{moment(record.update_time * 1000).
-                            format('YYYY-MM-DD HH:mm')}</Tag>),
-                }]}
-            expandable={{
-                expandRowByClick: true,
-                expandedRowRender: portServiceRowRender,
-                rowExpandable: record => record.portservice.length > 0, // expandIcon: ({ expanded, onExpand, record }) => null,
-            }}
-            dataSource={ipdomains}
-        />
+        <IPDomainTable/>
     </Fragment>)
 }
 export const IPDomainMemo = memo(IPDomain)
